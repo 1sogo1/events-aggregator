54756a1b333bb4d8ea8d7a8f84ed27cc671e5f6f
Maxim <sogo77468@gmail.com>
2026-04-20 12:29:16 +0700

    try fix db con

--- db/database.py
+++ db/database.py
@@ -4,12 +4,19 @@ import os
 
 load_dotenv()
 
+
+
 DB_URL = os.getenv('POSTGRES_CONNECTION_STRING')
 
 engine = create_async_engine(str(DB_URL), echo=False)
 
 Session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
+print(f"POSTGRES_CONNECTION_STRING: {os.getenv('POSTGRES_CONNECTION_STRING', 'NOT SET')}")
+print(f"DATABASE_URL: {os.getenv('DATABASE_URL', 'NOT SET')}")
+
 async def get_db():
     async with Session() as session:
         yield session
+
+    
